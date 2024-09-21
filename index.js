3624cdf76c708965fcfe8368603f24122c15d68e
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2024-09-21 16:43:34 +0530

    Chapter 1

---
+++ index.js
@@ -0,0 +1,30 @@
+// var age = 25 ;
+
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve()
+// {
+//     let a = 10;
+// }
+
+// console.log(a)
+
+// let a = 10;
+// a = "kanishk"
+// // a = true
+// console.log(a)
+
+function example() {
+    var x = 10;
+    console.log(x);
+}
+
+example();       // 10
+console.log(x);       // Throws ReferenceError: x is not defined
