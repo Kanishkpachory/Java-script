9fa470f601f3cc16f8ea5551e5a913122200cb57
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2024-09-24 21:16:11 +0530

    Function (Chapter-2)

---
+++ function.js
@@ -0,0 +1,44 @@
+
+// function sayMyname(){
+//     console.log("kanishk")
+// }
+
+// sayMyname()
+
+// function counting(){
+//     for(let i=1 ; i<=100 ;i++){
+//         console.log(i);
+//     }
+// }
+
+// counting()
+
+// function printNumber(num){
+//     console.log("printing number:" , num)
+// }
+
+// printNumber(5)
+
+// function average(num1 , num2){
+//     let average = (num1 +num2)/2;
+//     console.log("Average:" , average)
+// }
+
+// average(5,10)
+
+function sum(a , b, c){
+    let sum = a + b +c
+    return sum
+}
+
+let ans = sum(1,2,3)
+console.log(ans)
+
+function getMyname(first , last){
+    let fullname = first + " D " + last
+    return fullname
+
+}
+
+fullname = getMyname("kanishk" , "pachory")
+console.log(fullname)
