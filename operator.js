46f9631ae207c5a14edfbacbe2342450708b8d86
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2024-09-24 21:17:27 +0530

    Operator (Chapter-2)

---
+++ operator.js
@@ -0,0 +1,25 @@
+// let a = 10 ;
+// let b = 5;
+
+// console.log(a**=10)
+
+// console.log(a)
+
+// let age = 5;
+// let status1 = (age>18) ? 'I can vote ' : 'I cannot vote';
+
+// console.log(status1);
+
+// console.log(true || true || false)
+
+// console.log(true && true && true)
+
+// console.log(!true)
+
+// console.log(false && 'kanishk')
+// console.log(true && 3)
+// console.log(false || 'hello')
+
+// console.log(2 ^ 9)
+
+console.log(10 << 5)
