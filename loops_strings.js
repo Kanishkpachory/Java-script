92ec796427c2d3791dae701fc64d42e5841ac81d
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2024-09-24 21:16:56 +0530

    loops_strings (Chapter-2)

---
+++ loops_strings.js
@@ -0,0 +1,75 @@
+// for (let i=1 ; i<=10 ; i++) {
+//     console.log("kanishk")
+// }
+
+// for (let i=1 ; i<=10 ; i++) {
+//    console.log(i)
+// }
+
+// for (let i=5; i>=1 ; i--) {
+//     console.log(i)
+// }
+// for (let i=5; i>=1 ; i--) {
+//     if (i == 2){
+//         break;
+//     }
+//     console.log(i)
+// }
+// for (let i=5; i>=1 ; i--) {
+//     if (i == 2){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// let i = 1;
+// while(i<=10){
+//     console.log("kanishk");
+//     i++;
+// }
+// let i = 1;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+// i=1;
+// do{
+//     console.log(i)
+//     i++
+// }while(i<0)
+
+// let firstName = "kanishk"
+// let lastName = "Pachory"
+
+// let name = `this is kanishk
+// my name 
+// is ths only`
+// console.log(typeof(firstName))
+// console.log(name)
+
+// let firstname = new String("kanishk pachory");
+// console.log(firstname)
+
+// let op1 = "english ";
+// let op2 = "HINDI ";
+
+// console.log(op1.toUpperCase())
+// console.log(op2.toLocaleLowerCase())
+
+// console.log(op2.length)
+
+// let finalans = `${op1}${op2}`;
+// console.log(finalans)
+
+// let ans = op1 + op2;
+// console.log(ans);
+
+// let a = "kanishk"
+// console.log(a.substring(3,5))
+
+let sentence = "hello  kya kese ho sare ke sare";
+
+let word = sentence.split(' ');
+
+console.log(word);
