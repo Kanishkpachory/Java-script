9fd757cb1b9dcbf9581a299a6892fb3ea0fd69ab
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2024-09-28 21:59:45 +0530

    math_date_object(Chapter-6)

---
+++ math_date_object.js
@@ -0,0 +1,17 @@
+// console.log(Math.PI)
+// console.log(Math.max(20 ,30 ,40 ,50 ,60))
+// console.log(Math.min(20 , 30 , 40 , 50 , 5))
+// console.log(Math.round(1.49))
+// console.log(Math.floor(1.99))
+// console.log(Math.ceil(1.1))
+// console.log(Math.abs(-10))
+// console.log(Math.random())
+// console.log(Math.sqrt(4))
+// console.log(Math.pow(10,2))
+
+let cur = new Date()
+
+console.log(cur)
+
+let date = new Date('30 march 2002')
+console.log(date)
